3613a2a823f87d8c2de5134672c4b74f5d5118dd
zoyasarfraz <zoyas26870@gmqail.com>
2020-12-17 14:58:07 +0500

    signout

--- lib/widgets/chat_message.dart
+++ lib/widgets/chat_message.dart
@@ -9,14 +9,22 @@ class ChatMessage extends StatelessWidget{
 Widget build(BuildContext  context){
     final record =Record.fromMap(snapshot);
     return Container(
+margin: EdgeInsets.symmetric(vertical: 10.0),
       child: Row(
-        children: <Widget>[
-          CircleAvatar(child: Text(record.name[0]),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>
+      [ Container(
+      margin:EdgeInsets.only(right: 16.0),
+      child:CircleAvatar( backgroundImage: NetworkImage(record.avatarUrl),),
 
           ),
-
-          Text(record.text),
-        ],
+Container(decoration: BoxDecoration(
+  borderRadius: BorderRadius.all( Radius.circular(15.0)),
+color: Colors.deepOrange,
+),
+    child:Padding(padding: const EdgeInsets.all(15.0),
+    child: Text(record.text)),
+)],
       )
     );
 }
--- lib/widgets/chat_screen.dart
+++ lib/widgets/chat_screen.dart
@@ -1,6 +1,11 @@
 import 'package:chatapp/widgets/chat_list.dart';
 import 'package:chatapp/widgets/text_composer.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+enum MenuItem{
+  signout
+}
 class ChatScreen extends StatefulWidget {
   @override
   _ChatScreenState createState() => _ChatScreenState();
@@ -8,11 +13,35 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final List <Map<String ,dynamic>> _dummySnapshot=[];
+  final _googleSignIn = GoogleSignIn();
+  GoogleSignInAccount _currentUser;
+  @override
+  void inState(){
+    super.initState();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount account)
+    {
+setState(() {
+  _currentUser = account;
+});
+_googleSignIn.signInSilently();
+    });
+  }
+  Future<Null> _handleSignin() async{
+    var user = _googleSignIn.currentUser;
+    if (user == null)
+      user = await _googleSignIn.signInSilently();
+    if (user == null)
+      user = await _googleSignIn.signIn();
+  }
+  Future<void> _handleSignOut() async{
+    _googleSignIn.disconnect();
+
+}
   void _sendMessageCallback(String text){
     setState(() {
       _dummySnapshot.insert(0, {
-        'name': 'Zoya',
-       ' avatarUrl': '',
+        'name': '_googleSignIn.currentUser.displayName',
+       ' avatarUrl': '_googleSignIn.currentUser.photoUrl',
            'photoUrl': '',
         'text': text,
 
@@ -21,10 +50,44 @@ class _ChatScreenState extends State<ChatScreen> {
   }
   @override
   Widget build(BuildContext context) {
+    if (_currentUser==null)
+      return Scaffold(
+        backgroundColor: Colors.purple.shade400,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center ,
+          children:<Widget>  [
+            SignInButton(Buttons.Google, onPressed:() {
+              _handleSignin();
+
+            })
+          ],
+        ),
+      );
+    else
     return Scaffold(
       backgroundColor: Colors.purple.shade400,
       appBar: AppBar(
+
         title: Text('Coffee Chat'),
+        actions:  <Widget> [
+          PopupMenuButton<MenuItem>( onSelected: (
+MenuItem menuItem)
+          {
+            setState(()
+            {
+              _handleSignOut();
+            });
+          },
+            itemBuilder: (BuildContext context) =>
+            <PopupMenuEntry<MenuItem>>[
+              PopupMenuItem<MenuItem>(
+                value :MenuItem.signout,
+                child: Text('Google Signout'),
+              )
+            ],
+
+          ),
+        ],
       ),
       body: Column(
         children: <Widget> [
